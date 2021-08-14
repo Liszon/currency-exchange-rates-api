c98c27826bcab66b5dbf7865a3ccc2fa7f7441e2
Artur Liszewski <77889082+Liszon@users.noreply.github.com>
2021-08-14 11:54:24 +0200

    with restic backup

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 #Liszewski Artur
+#API-CRON-Backup
 import json
 import csv
 import requests
 import datetime
+import restic
 
-APIKEY='YOUR API KEY'
+#url = 'http://api.exchangeratesapi.io/v1/latest?access_key=a30a9d42aef5f4bd4ebe4a98cb7e2a6a&symbols=USD,AUD,CAD,PLN'
+
+APIKEY='a30a9d42aef5f4bd4ebe4a98cb7e2a6a'
 
 
 today = datetime.date.today() #getting the current date
 datelist = ['latest',"1","2","3","4","5","6"] #creating a list in which to place the dates
 #filling in a list of dates
 for i in range(1,7):
-    day = today+datetime.timedelta(days=0-i)
+    day = today+datetime.timedelta(days=0-(i+1))
     day = day.strftime("%Y-%m-%d")
     datelist[i] = day
 
@@ -33,7 +37,7 @@ for currency, rate in rates.items():
     currency0[i] = currency
     i=i+1
 
-#Day -1
+#Yesterday
 response = requests.get(urllist[1])
 data = response.text
 parsed = json.loads(data)
@@ -44,7 +48,7 @@ for currency, rate in rates.items():
     rate1[i] = rate
     i=i+1
 
-#Day -2
+#2 days ago
 response = requests.get(urllist[2])
 data = response.text
 parsed = json.loads(data)
@@ -55,7 +59,7 @@ for currency, rate in rates.items():
     rate2[i] = rate
     i=i+1
 
-#Day -3
+#3 days ago
 response = requests.get(urllist[3])
 data = response.text
 parsed = json.loads(data)
@@ -66,7 +70,7 @@ for currency, rate in rates.items():
     rate3[i] = rate
     i=i+1
 
-#Day -4
+#4 days ago
 response = requests.get(urllist[4])
 data = response.text
 parsed = json.loads(data)
@@ -77,7 +81,7 @@ for currency, rate in rates.items():
     rate4[i] = rate
     i=i+1
 
-#Day -5
+#5 days ago
 response = requests.get(urllist[5])
 data = response.text
 parsed = json.loads(data)
@@ -88,7 +92,7 @@ for currency, rate in rates.items():
     rate5[i] = rate
     i=i+1
 
-#Day -6
+#6 days ago
 response = requests.get(urllist[6])
 data = response.text
 parsed = json.loads(data)
@@ -102,7 +106,21 @@ for currency, rate in rates.items():
 #Saving the data in a CSV file
 with open('currency.csv', 'w',newline='') as file:
     writer=csv.writer(file, delimiter=';')
-    writer.writerow(["Currency", "Rate", "Day -1", "Day -2", "Day -3", "Day -4", "Day -5", "Day -6"])
+    writer.writerow(["Currency", "Rate", "Yesterday", "2 days ago", "3 days ago", "4 days ago", "5 days ago", "6 days ago"])
     writer.writerow(["EUR", 1, 1, 1, 1, 1, 1, 1])
     for i in range(4):
         writer.writerow([currency0[i], rate0[i], rate1[i], rate2[i], rate3[i], rate4[i], rate5[i], rate6[i]])
+
+#Creating backup
+restic.repository = '/backup'
+restic.password_file = 'password.txt'
+
+try:
+    restic.init()
+    print("Creating backup")
+except:
+    print("Backup alredy exist")
+
+restic.backup(paths=['currency.csv'])
+
+restic.restore(snapshot_id='latest', target_dir='restored')
